6e9a26001428e2f0e8eec75c91f580f34b27181e
toonkan <50565603+toonkan@users.noreply.github.com>
2020-09-29 02:38:15 -0700

    Add files via upload

---
+++ overloading1.java
@@ -0,0 +1,19 @@
+package week13b;
+
+public class overloading1 {
+
+	
+	static void sound() {
+		System.out.println("Hi");
+	}
+	static void sound(int a){
+		int cat;
+		cat = a;
+		System.out.println("Hey "+cat);
+	}
+	
+	
+	public static void main(String[]args) {
+		sound(59);
+	}
+}
